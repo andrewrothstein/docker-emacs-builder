e86c85cf203625e682172522a931586d4ec53f31
Andrew Rothstein <arothste@blackrock.com>
2015-11-10 12:31:35 -0500

    more comprehensive run script pulling the build package out of the docker-machine

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 export APP_NAME=emacs
+export APP_VER=24.5
+export APP_PLATFORM=x86_64
+export APP_PKG_TYPE=rpm
 export APP_TAG=centos_7.1.1503
-docker run --rm -v $(docker-machine ssh default pwd):/pkgdir andrewrothstein/docker-$APP_NAME-builder:$APP_TAG
+export APP_PKG=$APP_NAME-$APP_VER-$APP_PLATFORM.$APP_PKG_TYPE
+export DOCKER_MACHINE=default
+
+rm -f $APP_PKG
+docker-machine ssh $DOCKER_MACHINE -- rm -f $APP_PKG
+docker run --rm \
+       -v $(docker-machine ssh $DOCKER_MACHINE pwd):/pkgdir \
+       andrewrothstein/docker-$APP_NAME-builder:$APP_TAG
+docker-machine scp $DOCKER_MACHINE:$APP_PKG .
